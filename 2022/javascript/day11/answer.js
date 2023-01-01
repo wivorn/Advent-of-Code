23c3ed3331b871fb971682d98b70f3701ecc4201
Wivorn Chowattanakul <wivorn.c@gmail.com>
2023-01-01 14:38:35 -0500

    2022/11 part2 - js

--- 2022/javascript/day11/answer.js
+++ 2022/javascript/day11/answer.js
@@ -18,6 +18,7 @@ class Monkey {
                 return old * right
             }
         }
+        this.divisor = testParams[0]
         this.test = (val) => {
             const [divisor, trueMonkey, falseMonkey] = testParams
             return val % divisor === 0 ? trueMonkey : falseMonkey
@@ -27,17 +28,23 @@ class Monkey {
 }
 
 class MonkeyGame {
-    constructor(monkeys) {
+    constructor(monkeys, part) {
         this.monkeys = monkeys
         this.round = 0
+        this.part = part
+        this.divisor = monkeys.reduce((acc, m) => parseInt(m.divisor) * acc, 1)
     }
 
     startRound() {
-        monkeys.forEach(monkey => {
+        this.monkeys.forEach(monkey => {
             while (monkey.items.length) {
                 let item = monkey.items.shift()
                 item = monkey.operation(item)
-                item = Math.floor(item / 3)
+                if (this.part === 1) {
+                    item = Math.floor(item / 3)
+                } else {
+                    item = item % this.divisor
+                }
                 const nextMonkey = monkey.test(item)
                 monkey.inspectCount++
                 this.monkeys[nextMonkey].items.push(item)
@@ -66,7 +73,24 @@ const monkeys = puzzle.map(m => {
     return new Monkey(startingItems, operation, [divisor, trueValue, falseValue])
 })
 
-const game = new MonkeyGame(monkeys)
+const game = new MonkeyGame(monkeys, 1)
 game.run(20)
 const answer1 = game.monkeys.sort((a, b) => b.inspectCount - a.inspectCount).map(m => m.inspectCount).slice(0, 2).reduce((acc, curr) => acc * curr, 1)
 console.log(answer1)
+
+const monkeys2 = puzzle.map(m => {
+    let [, startingItems] = m.match(/items: (.*)/)
+    startingItems = startingItems.split(',').map((i) => parseInt(i.trim()))
+    let [, operation] = m.match(/new = (.*)/)
+    let [, divisor] = m.match(/divisible by (.*)/)
+    divisor = parseInt(divisor)
+    let [, trueValue] = m.match(/true: throw to monkey (.*)/)
+    trueValue = parseInt(trueValue)
+    let [, falseValue] = m.match(/false: throw to monkey (.*)/)
+    falseValue = parseInt(falseValue)
+    return new Monkey(startingItems, operation, [divisor, trueValue, falseValue])
+})
+const game2 = new MonkeyGame(monkeys2, 2)
+game2.run(10000)
+const answer2 = game2.monkeys.sort((a, b) => b.inspectCount - a.inspectCount).map(m => m.inspectCount).slice(0, 2).reduce((acc, curr) => acc * curr, 1)
+console.log(answer2)
